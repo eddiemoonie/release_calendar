099198eab5d5546f3f262e54b7e9083c1582df5b
Eddie Moon <eddiemoonie@gmail.com>
2018-05-01 21:55:33 +0000

    release_calendar

--- release_calendar/lib/release_calendar.rb
+++ release_calendar/lib/release_calendar.rb
@@ -1,4 +1,10 @@
-require "release_calendar/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "release_calendar/version"
+require_relative "release_calendar/cli"
+require_relative "release_calendar/release"
 
 module ReleaseCalendar
   # Your code goes here...
