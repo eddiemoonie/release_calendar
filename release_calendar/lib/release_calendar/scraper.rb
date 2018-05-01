819a8c073e29b7b64f0fc449734edc7ea36f4096
Eddie Moon <eddiemoonie@gmail.com>
2018-05-01 21:58:47 +0000

    start scraping

---
+++ release_calendar/lib/release_calendar/scraper.rb
@@ -0,0 +1,17 @@
+class ReleaseCalendar::Scraper
+
+  def get_page
+    Nokogiri::HTML(open("https://www.kicksonfire.com/app/"))
+  end
+
+  def scrape_sneakers_index
+    self.get_page.css("div.release-date-title")
+  end
+
+  def make_sneakers
+    scrape_sneakers_index.each do |s|
+      ReleaseCalendar::Release.new_from_index_page
+  end
+
+
+end
