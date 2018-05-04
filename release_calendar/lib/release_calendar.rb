9ef2dd62e8634ecbcaf62524acd8ce8248b88e21
Ki Moon <35046869+eddiemoonie@users.noreply.github.com>
2018-05-04 16:27:48 -0400

    Update release_calendar.rb

--- release_calendar/lib/release_calendar.rb
+++ release_calendar/lib/release_calendar.rb
@@ -5,7 +5,9 @@ require 'pry'
 require_relative "release_calendar/version"
 require_relative "release_calendar/cli"
 require_relative "release_calendar/release"
+require_relative "release_calendar/scraper"
 
 module ReleaseCalendar
   # Your code goes here...
 end
+
