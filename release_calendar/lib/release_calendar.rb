4175b891f3172e0bb080917402d3d6b6fd14d7cf
Ki Moon <35046869+eddiemoonie@users.noreply.github.com>
2018-05-07 14:21:54 -0400

    Update release_calendar.rb

--- release_calendar/lib/release_calendar.rb
+++ release_calendar/lib/release_calendar.rb
@@ -2,10 +2,10 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require_relative "release_calendar/version"
-require_relative "release_calendar/cli"
-require_relative "release_calendar/release"
-require_relative "release_calendar/scraper"
+require_relative "../lib/release_calendar/version"
+require_relative "../lib/release_calendar/cli"
+require_relative "../lib/release_calendar/release"
+require_relative "../lib/release_calendar/scraper"
 
 module ReleaseCalendar
   # Your code goes here...
